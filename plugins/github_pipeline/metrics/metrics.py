160050522f5420e48c3547d8492f5dc8f963d72e
Shivam Gupta <sgs.shivam99@outlook.com>
2025-12-25 20:17:50 +0530

    Update metrics.py

--- plugins/github_pipeline/metrics/metrics.py
+++ plugins/github_pipeline/metrics/metrics.py
@@ -1,1 +1,10 @@
+import os
 
+
+class PipelineMetrics:
+    def __init__(self, name: str):
+        self.file = f"{os.environ['AIRFLOW_HOME']}/logs/{name}_metrics.prom"
+
+    def increment(self, metric: str):
+        with open(self.file, "a") as f:
+            f.write(f"{metric} 1\n")
