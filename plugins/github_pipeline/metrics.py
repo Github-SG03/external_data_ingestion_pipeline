ddf3b155dfc1cc3b201c1c31acb363ea07228ae0
Shivam Gupta <sgs.shivam99@outlook.com>
2025-12-25 19:44:21 +0530

    Add PipelineMetrics class for logging metrics

---
+++ plugins/github_pipeline/metrics.py
@@ -0,0 +1,9 @@
+import os
+
+class PipelineMetrics:
+    def __init__(self, name):
+        self.file = f"{os.environ['AIRFLOW_HOME']}/logs/{name}_metrics.prom"
+
+    def increment(self, metric):
+        with open(self.file, "a") as f:
+            f.write(f"{metric} 1\n")
