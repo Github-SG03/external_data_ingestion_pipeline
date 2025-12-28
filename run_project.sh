ef8695e3670182810c3f7ffb795858fe95579d58
Shivam Gupta <sgs.shivam99@outlook.com>
2025-12-28 19:23:54 +0530

    commit the file changes

--- run_project.sh
+++ run_project.sh
@@ -10,49 +10,38 @@ source ~/airflow_env/bin/activate
 
 export AIRFLOW_HOME=/home/ec2-user/airflow
 export AIRFLOW__CORE__DAGS_FOLDER=$AIRFLOW_HOME/dags
-export PYTHONPATH=$AIRFLOW_HOME/src
 export ENV=dev
 
 echo "2️⃣ Prepare Airflow directories"
-mkdir -p $AIRFLOW_HOME/{dags,plugins,logs,src,config}
+mkdir -p $AIRFLOW_HOME/{dags,logs,src,config}
 
 echo "3️⃣ Install dependencies"
 pip install --no-cache-dir -r requirements.txt
 
-echo "4️⃣ Sync DAGs"
+echo "4️⃣ Install project package (IMPORTANT)"
+pip install -e .
+
+echo "5️⃣ Sync DAGs"
 rsync -av --delete dags/ $AIRFLOW_HOME/dags/
 
-echo "5️⃣ Sync src"
+echo "6️⃣ Sync src"
 rsync -av --delete src/ $AIRFLOW_HOME/src/
 
-echo "6️⃣ Sync config"
+echo "7️⃣ Sync config"
 rsync -av config/ $AIRFLOW_HOME/config/
 
-echo "7️⃣ Initialize / migrate Airflow DB"
+echo "8️⃣ Initialize / migrate Airflow DB"
 airflow db migrate
-
-echo "8⃣ Checking DAG import errors (FAIL FAST)"
-IMPORT_ERRORS=$(airflow dags list-import-errors | wc -l)
-
-if [ "$IMPORT_ERRORS" -gt 1 ]; then
-  echo "❌ DAG import errors detected. Aborting deployment."
-  airflow dags list-import-errors
-  exit 1
-else
-  echo "✅ No DAG import errors found"
-fi
 
 echo "9️⃣ Kill old Airflow"
 pkill -f "airflow scheduler" || true
 pkill -f "airflow webserver" || true
 sleep 5
 
-echo "🔟 Start Airflow"
+echo "🔟 Start Airflow services"
 nohup airflow scheduler > $AIRFLOW_HOME/scheduler.log 2>&1 &
 nohup airflow dag-processor > $AIRFLOW_HOME/dag-processor.log 2>&1 &
 nohup airflow api-server --port 8080 > $AIRFLOW_HOME/api-server.log 2>&1 &
-
-sleep 20
 
 echo "⏳ Waiting for Airflow API server..."
 for i in {1..50}; do
@@ -60,19 +49,18 @@ for i in {1..50}; do
     echo "✅ Airflow API server is healthy"
     break
   fi
-  echo "⏳ Not ready yet... retry $i/50"
   sleep 5
 done
 
-
-# final safety check
-ss -lntp | grep -q 8080 || {
-  echo "❌ Airflow API server failed to start after 250 seconds"
+echo "🧪 Checking DAG import errors"
+IMPORT_ERRORS=$(airflow dags list-import-errors | wc -l)
+if [ "$IMPORT_ERRORS" -gt 1 ]; then
+  airflow dags list-import-errors
+  echo "❌ DAG import errors found"
   exit 1
-}
+fi
 
-
-
+echo "✅ Deployment successful"
 
 #######################################################PROJECT EXECUTION STEPS################################
 
